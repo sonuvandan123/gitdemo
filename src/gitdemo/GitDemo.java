7b3ee9396be78e82770c3898558f5d49c5f82036
sonuvandan <collegetoindustry@gmail.com>
2020-08-02 19:50:03 +0530

    code by second dev

--- src/gitdemo/GitDemo.java
+++ src/gitdemo/GitDemo.java
@@ -5,6 +5,7 @@ public class GitDemo {
 	public static void main(String[] args) {
 System.out.println("Hello Git");
 System.out.println("New line of code added");
+System.out.println("code added by 2nd developer");
 	}
 
 }
---
+++ src/gitdemo/SecondDev.java
@@ -0,0 +1,9 @@
+package gitdemo;
+
+public class SecondDev {
+
+	public static void main(String[] args) {
+System.out.println("2nd developer");
+	}
+
+}
